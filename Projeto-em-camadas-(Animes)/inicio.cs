f543feb4ae5c2f669d9412bcff92c1ee307c904b
Enzo Rafael <enzoricarteaa@gmail.com>
2021-10-27 08:25:58 -0300

    Create inicio.cs

---
+++ Projeto-em-camadas-(Animes)/inicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_em_camadas__Animes_
+{
+    public partial class inicio : Form
+    {
+        public inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
